b234399973e4f3ce81130ba51f6a259e5c9eba1e
Myles Sanigar <myles.sanigar@gmail.com>
2022-11-04 22:47:17 +0000

    firebase issues

--- server/authToken.ts
+++ server/authToken.ts
@@ -1,7 +1,18 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccount.json');
 
-admin.initializeApp({credential: admin.credential.cert(serviceAccount),
+admin.initializeApp({credential: admin.credential.cert({
+  "type": "service_account",
+  "project_id": "keepr-4cc8f",
+  "private_key_id": process.env.FIREBASE_ID,
+  "private_key": process.env.FIREBASE_KEY,
+  "client_email": "firebase-adminsdk-2c8m1@keepr-4cc8f.iam.gserviceaccount.com",
+  "client_id": "100975846486169039626",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2c8m1%40keepr-4cc8f.iam.gserviceaccount.com"
+}
+),
       databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
