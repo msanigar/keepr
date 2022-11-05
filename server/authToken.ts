1e6df1c2a1850b3c9ad032c0d8dcae9fdbb58967
Myles Sanigar <myles.sanigar@gmail.com>
2022-11-05 16:04:53 +0000

    fix env vars

--- server/authToken.ts
+++ server/authToken.ts
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
+require('dotenv').config();
 
 admin.initializeApp({credential: admin.credential.cert({
   "type": "service_account",
   "project_id": "keepr-4cc8f",
   "private_key_id": process.env.FIREBASE_ID,
-  "private_key": process.env.FIREBASE_KEY,
+  "private_key": process.env.FIREBASE_KEY.replace(/\\n/g, '\n'),
   "client_email": "firebase-adminsdk-2c8m1@keepr-4cc8f.iam.gserviceaccount.com",
   "client_id": "100975846486169039626",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
